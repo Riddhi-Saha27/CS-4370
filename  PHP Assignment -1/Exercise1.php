c4ab053860bab6c11d83c251f1a4550089271d42
Riddhi_Saha27 <riddhisaha123@gmail.com>
2024-10-13 23:18:16 -0400

    Exercise 1 php assignment added

---
+++  PHP Assignment -1/Exercise1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <?php
+    function hello_world() {
+        echo "Hello world!";
+    }
+
+    // Call the function to display the message
+    hello_world();
+    ?>
+</body>
+</html>

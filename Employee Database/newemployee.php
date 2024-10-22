0dbbd280856f0867e7c4a02d01217d6734d49f7e
Riddhi_Saha27 <riddhisaha123@gmail.com>
2024-10-22 14:30:32 -0400

    php form created to take in user inputs and route to viewreview.php

---
+++ Employee Database/newemployee.php
@@ -0,0 +1,8 @@
+<form action="viewemployees.php" method="post">
+   Employee Name: <input type="text" name="emp_name"><br>
+   Job Title: <input type="text" name="job_title"><br>
+   Hire Date: <input type="date" name="hire_date"><br>
+   Salary: <input type="text" name="salary"><br>
+   Department ID: <input type="text" name="dept_id"><br>
+   <input type="submit" value="Add Employee">
+</form>
